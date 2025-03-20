36ffb41c33369f77af87bb3265c14f5aa361a70b
Pavithra23-05 <pavithravinothpa23@gmail.com>
2025-03-20 15:32:15 +0530

    third commit

--- src/Basics/sample1.java
+++ src/Basics/sample1.java
@@ -2,8 +2,17 @@ package Basics;
 
 public class sample1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("git add -A - staging area");
+		
+		System.out.println("git commit -m 'commit message' staging area to local repo");
+		
+		System.out.println("git push -u orgin master - git to github");
+		
+		
+		
+		System.out.println("Git Tutorial");
 
 	}
 
