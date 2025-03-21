6f5c66e1f43c6d649616b8177b31db760443c5b6
veelinktech <veelinktechonoliges@gmail.com>
2025-03-21 15:13:30 +0530

    veelinktech 2nd commit

--- src/Basics/Sample2.java
+++ src/Basics/Sample2.java
@@ -1,9 +1,28 @@
 package Basics;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Sample2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		System.out.println(driver.getTitle());
+		
+		driver.close();
+		
+		
 
 	}
 
